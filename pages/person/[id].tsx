04a9557d3abb944e365115519bfbed26ef80c01f
Амиш Любимов <amish_lubimov@mail.ru>
2022-09-09 12:02:28 +0300

    tweaks

--- pages/person/[id].tsx
+++ pages/person/[id].tsx
@@ -1,13 +1,10 @@
 import { useRouter } from "next/router";
-import { FC } from "react";
 import Person from "../../src/components/page components/Person Page/Person";
 
-interface personPageProps {}
-
-const personPage: FC<personPageProps> = () => {
+const PersonPage = () => {
 	const { id } = useRouter().query;
 	console.log(id);
 	return <Person id={id} />;
 };
 
-export default personPage;
+export default PersonPage;
